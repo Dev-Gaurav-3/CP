f239e08e132b56f0d4d175fadf21fdced5a61f2d
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2026-02-11 18:32:17 +0530

    add

---
+++ C_Good_Subarrays.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long
+#define MOD 1000000007
+
+#define returnNO  {std::cout << "NO"  << std::endl; return;}
+#define returnYES {std::cout << "YES" << std::endl; return;}
+#define return0 {std::cout << "0" << std::endl; return;}
+#define return1 {std::cout << "1" << std::endl; return;}
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+
+template <typename T>
+void input(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (auto &x : v)
+        cin >> x;
+}
+
+void f()
+{
+    int n;cin>>n;
+    string s;cin>>s;
+    vector<int>pref(n+1);
+    for(int i = 1;i<=n;i++){
+        pref[i] = pref[i-1] + (s[i-1] - '0');
+    } 
+    map<ll,ll>mp;
+    ll ans = 0;
+    for (int i = 0; i <=n; i++)
+    {
+        ll val = pref[i] - i;
+        ans += mp[val];
+        mp[val]++;
+    }
+    cout<< ans <<endl;
+    
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        f();
+    }
+    return 0;
+}
