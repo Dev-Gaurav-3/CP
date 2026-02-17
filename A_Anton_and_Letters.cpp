32385c82640e0e73dc540c66596dc495c59f8d2b
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2026-02-17 16:01:26 +0530

    add

---
+++ A_Anton_and_Letters.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long
+#define MOD 1000000007
+
+#define returnNO  {std::cout << "NO"  << std::endl; return;}
+#define returnYES {std::cout << "YES" << std::endl; return;}
+#define return1 {std::cout << "1" << std::endl; return;}
+#define return0 {std::cout << "0" << std::endl; return;}
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+
+template <typename T>
+void input(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (auto &x : v)
+        cin >> x;
+}
+
+void f()
+{
+    char c;
+    set<int>st;
+    while(cin>>c)if(c >= 'a' && c <= 'z')st.insert(c);
+    cout<< st.size() <<endl;
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        f();
+    }
+    return 0;
+}
---
+++ B_Erase_First_or_Second_Letter.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long
+#define MOD 1000000007
+
+#define returnNO  {std::cout << "NO"  << std::endl; return;}
+#define returnYES {std::cout << "YES" << std::endl; return;}
+#define return1 {std::cout << "1" << std::endl; return;}
+#define return0 {std::cout << "0" << std::endl; return;}
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+
+template <typename T>
+void input(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (auto &x : v)
+        cin >> x;
+}
+
+void f()
+{
+    int n;cin >> n;
+    string s;cin >> s;
+
+    ll ans = 0;
+    vector<bool> visited(26, false);
+
+    for (int i = 0; i < n; ++i) {
+        int j = s[i] - 'a';
+        if (!visited[j]) {
+            ans += (n - i);
+            visited[j] = true;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        f();
+    }
+    return 0;
+}
