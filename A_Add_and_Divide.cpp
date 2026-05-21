dda164ae4163cd1d3eecd5b4d4dce9140680ccf4
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2026-05-21 18:00:44 +0530

    add

---
+++ A_Add_and_Divide.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long
+#define MOD 1000000007
+
+#define returnNO  {std::cout << "NO"  << std::endl; return;}
+#define returnYES {std::cout << "YES" << std::endl; return;}
+#define return1 {std::cout << "1" << std::endl; return;}
+#define return0 {std::cout << "0" << std::endl; return;}
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+
+template <typename T>
+void input(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (auto &x : v)
+        cin >> x;
+}
+
+void f()
+{
+    int a,b;cin>>a>>b;
+    if(b>a) return1;
+    ll ans = 1e18;
+
+    for (int i = 0; i < 31; i++)
+    {
+        ll x = i + b;
+        if (x == 1) continue;
+
+        ll temp = a;
+        ll curr = i;
+        while(temp > 0){
+            temp /= x;
+            curr++;
+        }
+        ans = min(ans,curr);
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        f();
+    }
+    return 0;
+}
---
+++ A_Optimal_Purchase.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long
+#define MOD 1000000007
+
+#define returnNO  {std::cout << "NO"  << std::endl; return;}
+#define returnYES {std::cout << "YES" << std::endl; return;}
+#define return1 {std::cout << "1" << std::endl; return;}
+#define return0 {std::cout << "0" << std::endl; return;}
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+
+template <typename T>
+void input(vector<T> &v, int n)
+{
+    v.resize(n);
+    for (auto &x : v)
+        cin >> x;
+}
+
+void f()
+{
+    ll n,a,b;cin>>n>>a>>b;
+    if(3*a<=b){
+        cout << a*n << endl;
+    }
+    else{
+        ll x = n%3,ans = b*(n/3);
+        if(x == 2){
+            ans += min(2*a,b);
+        }
+        if(x == 1){
+            ans += min(a,b);
+        }
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        f();
+    }
+    return 0;
+}
